3c9fab78c0f4c063eda7b3ee22f6b0d5a5d6104b
Aravind Kotikelapudi <askotikelapudi@ucdavis.edu>
2022-11-16 17:11:25 -0800

    Copying rewards/losses to cpu before appending

--- dqn_training.py
+++ dqn_training.py
@@ -76,7 +76,7 @@ class training():
     
             if done:
                 state = self.environment.reset()
-                self.all_rewards.append(episode_reward)
+                self.all_rewards.append(episode_reward.cpu())
 
                 if tensorboard:
                     writer.add_scalar('Episode rewards', episode_reward, frame_idx)
@@ -86,7 +86,7 @@ class training():
         
             if len(self.replay_buffer) > batch_size:
                 loss = self.compute_td_loss(batch_size)
-                self.losses.append(loss.data)
+                self.losses.append(loss.data.cpu())
 
                 if tensorboard:
                     writer.add_scalar('Episode Losses', loss.data, frame_idx)
