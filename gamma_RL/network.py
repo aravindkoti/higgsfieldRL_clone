96a2a1ad9407f63fa1a5217dd07a7178dfc56dc0
Aravind Kotikelapudi <askotikelapudi@ucdavis.edu>
2022-12-02 10:28:05 -0800

    Customizable starting gamma

--- gamma_RL/network.py
+++ gamma_RL/network.py
@@ -44,14 +44,15 @@ class gamma_DQN(nn.Module):
 
 #Going to implement a separate nn for gamma in this model
 class gamma_DQN_epsilonseed(nn.Module):
-    def __init__(self, num_inputs, num_actions, environment, device, Variable):
+    def __init__(self, num_inputs, num_actions, environment, device, Variable, gamma):
         self.environment = environment
         self.Variable = Variable
         self.device = device
         
 
         super(gamma_DQN_epsilonseed, self).__init__()
-        self.gamma = nn.Parameter(torch.tensor(random.uniform(0.5,1)), requires_grad=True)
+        # self.gamma = nn.Parameter(torch.tensor(random.uniform(0.5,1)), requires_grad=True)
+        self.gamma = nn.Parameter(torch.tensor(gamma), requires_grad=True)
         
         self.layers = nn.Sequential(
             nn.Linear(self.environment.observation_space.shape[0], 128),
